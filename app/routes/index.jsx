605ec639dbc49d117c4c666ec64ff591cddd4bcb
Archit Jain <architjain798@gmail.com>
2025-05-17 18:03:42 +0530

    Refactor index page to enhance content and add styles for better presentation

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,23 @@
-import { Link } from '@remix-run/react';
+import { Link } from "@remix-run/react";
+import homeStyles from "../styles/home.css";
 
 export default function Index() {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Link to="/notes">Go to Notes Page</Link>
-    </>
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our new notes app built with Remix and Javascript</p>
+      <p id="cta">
+        <Link to="/notes">Go to Notes Page</Link>
+      </p>
+    </main>
   );
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: homeStyles,
+    },
+  ];
 }
