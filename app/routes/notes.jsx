7b3ab4f9a73260ea728417ceef8f42a2abcb7fc9
Archit Jain <architjain798@gmail.com>
2025-05-17 17:23:31 +0530

    Add styles and create Notes page with navigation link

---
+++ app/routes/notes.jsx
@@ -0,0 +1,9 @@
+export default function Notes() {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <p>This is the notes page.</p>
+      <p>Here you can add, view, and manage your notes.</p>
+    </div>
+  );
+}
