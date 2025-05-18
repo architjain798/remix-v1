2c94e921695dae2b46120e0b47260a9636e681af
Archit Jain <architjain798@gmail.com>
2025-05-18 18:05:49 +0530

    Add NoteList component and styles for displaying notes

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,13 +1,23 @@
 import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
+import NoteList, { links as notesListLink } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function Notes() {
+  const notes = useLoaderData();
+
   return (
-    <div>
+    <main>
       <NewNote />
-    </div>
+      <NoteList notes={notes} />
+    </main>
   );
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
 }
 
 export async function action({ request }) {
@@ -32,5 +42,5 @@ export async function action({ request }) {
 }
 
 export function links() {
-  return [...NewNoteLinks()];
+  return [...NewNoteLinks(), ...notesListLink()];
 }
