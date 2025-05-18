08f317935b6927fde495f0c257599ad93532c5c4
Archit Jain <architjain798@gmail.com>
2025-05-18 17:09:33 +0530

    Add note storage functionality and update NewNote component styles

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function Notes() {
   return (
@@ -6,6 +8,27 @@ export default function Notes() {
       <NewNote />
     </div>
   );
+}
+
+export async function action({ request }) {
+  console.log("Action called in notes route");
+  console.log("Received data:", request);
+
+  const formData = await request.formData();
+  // const noteData = {
+  //   title: formData.get("title"),
+  //   content: formData.get("content"),
+  // };
+  const noteData = Object.fromEntries(formData);
+  // Add the validation logic here
+
+  const exisitingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = exisitingNotes.concat(noteData);
+  console.log("Updated notes:", updatedNotes);
+  await storeNotes(updatedNotes);
+
+  return redirect("/notes");
 }
 
 export function links() {
