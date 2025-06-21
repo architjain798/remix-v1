0cbba091831031d2208e5f6db817f2caf82a8bc6
Archit Jain <architjain798@gmail.com>
2025-06-21 21:31:53 +0530

    Refactor NoteDetailsPage to load and display individual note details with error handling

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -1,18 +1,34 @@
-import { NavLink } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { NavLink, useLoaderData } from "@remix-run/react";
 import styles from "app/styles/note-details.css";
+import { getStoredNotes } from "../data/notes";
 
 export default function NoteDetailsPage() {
+  const selectedNote = useLoaderData();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <NavLink to="/notes">Back to Notes</NavLink>
         </nav>
-        <h1>NOTE TITLE</h1>
+        <h1>{selectedNote?.title}</h1>
       </header>
-      <p id="note-details-content">NOTE CONTENT</p>
+      <p id="note-details-content">{selectedNote?.content}</p>
     </main>
   );
+}
+
+export async function loader({ params }) {
+  const notes = await getStoredNotes();
+
+  const selectedNote = notes.find((note) => note.id === params.noteId);
+
+  if (!selectedNote) {
+    throw json({ message: "Note not found" }, { status: 404 });
+  }
+
+  return selectedNote;
 }
 
 export function links() {
@@ -22,4 +38,11 @@ export function links() {
       href: styles,
     },
   ];
+}
+
+export function meta({ data }) {
+  return {
+    title: data?.title || "Remix Notes",
+    description: data?.description || "A simple notes app built with Remix",
+  };
 }
